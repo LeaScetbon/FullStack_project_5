96a9379af65a6ea2ce4d1792e6ecada6a79543a6
LeaScetbon <73163251+LeaScetbon@users.noreply.github.com>
2023-05-09 17:32:39 +0300

    starting post

--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -1,8 +1,34 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 function Posts() {
+  const [posts, setPosts] = useState([]);
+  const navigate = useNavigate();
+ 
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => setPosts(data))
+      .catch((error) => {
+        console.error("Error fetching todos:", error);
+        navigate("/error");
+      });
+  }, []);
+  
+  const postsOfUser = posts.filter((post) => post.userId === JSON.parse(localStorage.getItem("currentUser")).id);
+
+
   return (
+    
     <section className="section">
-      <h4>Posts</h4>
-    </section>
+    <h4>Posts</h4>
+    {postsOfUser.map((post) => (
+      <div key={post.id}>
+       
+        <label>{post.title}</label>
+      </div>
+    ))}
+  </section>
   );
 }
 
