102b4cc24fb7668c2ec673191b793f84c91eee56
avtessle <avigayiltess@gmail.com>
2023-05-19 11:50:19 +0300

    info+albums

--- src/pages/AlbumPhotos.js
+++ src/pages/AlbumPhotos.js
@@ -1,8 +1,89 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import styles from "./AlbumPhotos.module.css";
+
 function AlbumPhotos() {
+  const [photos, setPhotos] = useState([]);
+  const [startIndex, setStartIndex] = useState(0);
+  const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0);
+  const [endOfAlbum, setEndOfAlbum] = useState(false);
+  const [endOfSet, setEendOfSet] = useState(false);
+
+  const location = useLocation();
+  const { title } = location.state;
+  const { albumId } = useParams();
+  const limit = 10;
+
+  useEffect(() => {
+    let url = `https://jsonplaceholder.typicode.com/photos?albumId=${albumId}&_start=${startIndex}&_limit=${limit}`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setPhotos((prevPhotos) => [...prevPhotos, ...data]);
+        if (data.length < limit) {
+          setEndOfAlbum(true);
+        }
+        if (endOfSet) {
+          setCurrentPhotoIndex((prevIndex) => prevIndex + 1);
+          setEendOfSet(false);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching photos:", error);
+        return [];
+      });
+  }, [startIndex]);
+
+  const handlePrevious = () => {
+    setCurrentPhotoIndex((prevIndex) => prevIndex - 1);
+    setEndOfAlbum(false);
+  };
+
+  const handleNext = () => {
+    if (currentPhotoIndex === photos.length - 1) {
+      setStartIndex((prevStartIndex) => prevStartIndex + limit);
+      setEendOfSet(true);
+    } else {
+      setCurrentPhotoIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
   return (
-    <section className="section">
-      <h4>AlbumPhotos</h4>
-    </section>
+    <div className={styles.slider}>
+      <h1 className={styles.albumTitle}>{title}</h1>
+      {photos.length > currentPhotoIndex ? (
+        <div className={styles.photoWrapper}>
+          <button
+            onClick={handlePrevious}
+            disabled={currentPhotoIndex === 0}
+            className={`${styles["arrow-button"]} ${styles.leftArrow}`}
+          >
+            &lt;
+          </button>
+          <div className={styles.photoContent}>
+            <img
+              src={photos[currentPhotoIndex].thumbnailUrl}
+              alt={photos[currentPhotoIndex].title}
+              className={styles.photo}
+            />
+            <h3 className={styles.photoTitle}>
+              {photos[currentPhotoIndex].title}
+            </h3>
+          </div>
+          <button
+            onClick={handleNext}
+            disabled={endOfAlbum || endOfSet || photos.length === 0}
+            className={`${styles["arrow-button"]} ${styles.rightArrow}`}
+          >
+            &gt;
+          </button>
+        </div>
+      ) : (
+        <p>{endOfAlbum ? "End of album" : "Loading..."}</p>
+      )}
+    </div>
   );
 }
 
--- src/pages/Albums.js
+++ src/pages/Albums.js
@@ -1,5 +1,6 @@
+import styles from "./Albums.module.css";
 import { useState, useEffect } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom/dist";
 
 function Albums() {
   const [albums, setAlbums] = useState([]);
@@ -8,26 +9,39 @@ function Albums() {
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/albums")
       .then((response) => response.json())
-      .then((data) => setAlbums(data))
+      .then((data) =>
+        setAlbums(data.filter((album) => album.userId === user.id))
+      )
       .catch((error) => {
         console.error("Error fetching albums:", error);
-        // Navigate("/error");
       });
   }, []);
 
-  const userAlbums = albums.filter((album) => album.userId === user.id);
-
   return (
-    <div>
-      {userAlbums.map((album) => (
-        <div key={album.id}>
-          <NavLink key={album.id} to={`/albums/${album.id}`}>
+    <div className={styles.albumList}>
+      {albums.map((album) => (
+        <div className={styles.albumItem} key={album.id}>
+          <NavLink
+            to={`/albums/${album.id}`}
+            state={{ title: album.title }}
+            className={styles.albumLink}
+            // activeClassName={styles.activeLink}
+          >
             {album.title}
           </NavLink>
         </div>
       ))}
     </div>
+
+    // <div>
+    //   {albums.map((album) => (
+    //     <div key={album.id}>
+    //       <NavLink to={`/albums/${album.id}`} state={{ title: album.title }}>
+    //         {album.title}
+    //       </NavLink>
+    //     </div>
+    //   ))}
+    // </div>
   );
 }
-
 export default Albums;
